4120cd7983d2b4245bb305d832a24b50471d070b
Brandon <bfree98@icloud.com>
2023-05-20 02:09:08 -0400

    refined logic added framework for upvoting system

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const db = require('./database.js')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 
 const hostIp = '10.0.0.248'
 const http_port = 8000
@@ -9,12 +10,13 @@ const http_port = 8000
 app.use(cors({
     origin: `http://${hostIp}:3000`
 }))
+app.use(bodyParser.json())
 
 app.listen(http_port, () => {
     console.log(`Server running on port ${http_port}`);
 });
 
-app.get("/api/requests", (req, res,) => {
+app.get("/api/requests", (req, res) => {
     const sql = "select * from ideaTable"
     const params = []
     db.all(sql, params, (err, rows) => {
@@ -28,6 +30,28 @@ app.get("/api/requests", (req, res,) => {
         })
     });
 });
+
+app.post("/api/upvote", (req, res) => {
+    const { id, upvotes } = req.body;
+    const sql = "UPDATE ideaTable SET upvotes = ? WHERE uid = ?";
+    const params = [upvotes, id];
+
+    db.run(sql, params, function (err) {
+        if (err) {
+            res.status(400).json({ "error": err.message });
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": {
+                id: id,
+                upvotes: upvotes
+            }
+        });
+    });
+});
+
+
 app.use(function (req, res) {
-    res.status(404);
+    res.status(404).send("404 - Not Found");
 });
