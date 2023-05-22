adba943205f6e2f1641307e9a08d53a24f309e33
RoyStorey <imlejiii@gmail.com>
2023-05-22 03:52:51 -0400

    Bonko Requests

--- server/server.js
+++ server/server.js
@@ -4,10 +4,10 @@ const db = require('./database.js')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
-const http_port = 8000
+const http_port = 8420
 
 app.use(cors({
-    origin: 'http://10.0.0.248:3000'
+    origin: 'http://192.168.0.192:3000'
 }))
 
 app.use(bodyParser.json())
